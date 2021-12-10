ba166c46958321fa0d26bf8d8f7320444884c63c
Dragibus <jeremiecool76700@hotmail.fr>
2021-12-10 09:39:52 +0100

    2nd Menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import SimpleBottomNavigation from "./components/MainNav";
+import SimpleBottomNavigation2 from "./components/MainNav2";
 import Movies from "./Pages/Movies/Movies";
 import Series from "./Pages/Series/Series";
 import Trending from "./Pages/Trending/Trending";
 import Search from "./Pages/Search/Search";
 import Home from "./Pages/Home/Home";
 import { Container } from "@material-ui/core";
-import Nav from "./components/Nav"
 import Connexion from "./components/Connexion";
+import Apropos from "./Pages/Apropos/Apropos";
+import Watchlist from "./Pages/Watchlist/Watchlist";
 
 
 
@@ -16,7 +18,7 @@ function App() {
 
   return (
     <BrowserRouter>
-     <Nav/>
+     
       <div className="app">
         <Container>
         
@@ -27,10 +29,13 @@ function App() {
             <Route path="/Series" component={Series} />
             <Route path="/Rechercher" component={Search} />
             <Route path="/Connexion" component={Connexion} />
+            <Route path="/Apropos" component={Apropos} />
+            <Route path="/Watchlist" component={Watchlist} />
           </Switch>
         </Container>
       </div>
       <SimpleBottomNavigation />
+      <SimpleBottomNavigation2 />
     </BrowserRouter>
   );
 }
--- src/components/MainNav2.js
+++ src/components/MainNav2.js
@@ -2,42 +2,34 @@ import React, { Fragment, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import TvIcon from "@material-ui/icons/Tv";
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import MovieIcon from "@material-ui/icons/Movie";
-import SearchIcon from "@material-ui/icons/Search";
-import WhatshotIcon from "@material-ui/icons/Whatshot";
 import { useHistory } from "react-router-dom";
-import HomeIcon from '@mui/icons-material/Home';
 import LoginIcon from '@mui/icons-material/Login';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
 import "../components/mainNav.css"
 const useStyles = makeStyles({
   root: {
     width: "100%",
     position: "fixed",
-    top: 0,
+    bottom: 0,
     backgroundColor: "#051724",
     zIndex: 100,
   },
   
 });
 
-export default function SimpleBottomNavigation() {
+export default function SimpleBottomNavigation2() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
   const history = useHistory();
 
   useEffect(() => {
     if (value === 0) {
-      history.push("/Home");
+      history.push("/Apropos");
     } else if (value === 1) {
-      history.push("/Tendance");
+      history.push("/Watchlist");
     } else if (value === 2) {
-      history.push("/Films");
-    } else if (value === 3) {
-      history.push("/Series");
-    } else if (value === 4) {
-      history.push("/Rechercher");
+      history.push("/Connexion");
     } 
   }, [value, history]);
 
@@ -56,37 +48,24 @@ export default function SimpleBottomNavigation() {
     >
       <BottomNavigationAction
         style={{ color: "#54DECE" }}
-        label="Accueil"
-        icon={<HomeIcon />}
+        label="Apropos"
+        icon={<MenuBookIcon />}
       />
       <BottomNavigationAction
         style={{ color: "#54DECE" }}
-        label="Tendance"
-        icon={<WhatshotIcon />}
+        label="WatchList"
+        icon={<LiveTvIcon />}
       />
       <BottomNavigationAction
         style={{ color: "#54DECE" }}
-        label="Films"
-        icon={<MovieIcon />}
+        label="Connexion"
+        icon={<LoginIcon />}
       />
-      <BottomNavigationAction
-        style={{ color: "#54DECE" }}
-        label="Series"
-        icon={<TvIcon />}
-      />
-       <BottomNavigationAction
-        style={{ color: "#54DECE" }}
-        label="Rechercher"
-        icon={<SearchIcon />}
-      />
-      
+
     </BottomNavigation>
    
     <BottomNavigationAction />
-    <div>
-      <p>Développer avec <FavoriteIcon className="coeur" /> par </p>
-      <img src="https://zupimages.net/up/21/49/d21v.png" className="logo_sign"></img>
-    </div>
+    
       
     
     </Fragment>
